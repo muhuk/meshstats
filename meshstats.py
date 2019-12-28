3a1b7978c27b0b3148941ea7f5f798ed20bfdc75
Atamert Ölçgen <muhuk@muhuk.com>
2019-12-28 20:07:44 +0300

    Separate stats calculation with draw code

--- meshstats.py
+++ meshstats.py
@@ -1,8 +1,8 @@
 # <pep8-80 compliant>
 
-import bpy.utils
-import bpy.types
+import bpy
 import bmesh
+import time
 
 
 bl_info = {
@@ -26,29 +26,54 @@ class MeshstatsPanel(bpy.types.Panel):
     bl_idname = "VIEW3D_PT_meshstats"
     bl_label = "Meshstats"
 
-    def draw(self, context):
-        obj = context.active_object
-        self.layout.label(text="Selected object: {}".format(obj.name))
-
-        bm = bmesh.new()
-        bm.from_mesh(obj.data)
-
-        self.layout.label(text="# of faces = {}".format(len(bm.faces)))
-
-        tris = [f for f in bm.faces if len(f.loops) == 3]
-        quads = [f for f in bm.faces if len(f.loops) == 4]
-        ngons = [f for f in bm.faces if len(f.loops) > 4]
-        self.layout.label(text="# of tris = {}".format(len(tris)))
-        self.layout.label(text="# of quads = {}".format(len(quads)))
-        self.layout.label(text="# of ngons = {}".format(len(ngons)))
-
-        bm.free()
-
     @classmethod
     def poll(cls, context):
         return  context.mode == 'OBJECT' \
             and context.selected_objects \
             and context.active_object.type == 'MESH'
+
+    def draw(self, context):
+        obj = context.active_object
+        self.layout.label(text="Selected object: {}".format(obj.name))
+        data = {}
+        self._calculate_stats(data, obj)
+        self._draw_summary_table(self.layout, data)
+
+    @staticmethod
+    def _draw_summary_table(layout, data):
+        j = layout.grid_flow(columns = 3)
+        j.label(text="")
+        j.label(text="Tris")
+        j.label(text="Quads")
+        j.label(text="Ngons")
+
+        j.label(text="count")
+        j.label(text="{}".format(data['tris_count']))
+        j.label(text="{}".format(data['quads_count']))
+        j.label(text="{}".format(data['ngons_count']))
+
+        j.label(text="percentage")
+        j.label(text="???")
+        j.label(text="???")
+        j.label(text="???")
+
+    @staticmethod
+    def _calculate_stats(d, obj):
+        start_time = time.time()
+        bm = bmesh.new()
+        bm.from_mesh(obj.data)
+
+        tris = [f for f in bm.faces if len(f.loops) == 3]
+        quads = [f for f in bm.faces if len(f.loops) == 4]
+        ngons = [f for f in bm.faces if len(f.loops) > 4]
+
+        d['tris_count'] = len(tris)
+        d['quads_count'] = len(quads)
+        d['ngons_count'] = len(ngons)
+
+        bm.free()
+        duration = time.time() - start_time
+        print("calculated stats for '{}' in {:.4f} seconds".format(obj.name, duration))
 
 
 def register():
